a3752037959a8574a4d2c50dc0121530531b8b9e
KekaPeka <maksim.kripinevich@rambler.ru>
2018-03-01 19:00:00 +0300

    adafs

---
+++ src/index.js
@@ -0,0 +1,16 @@
+module.exports = function getZerosCount(number)
+ {
+  module.exports = function getZerosCount(number)
+ {
+	var n = 5;
+	var res = 0;
+	while(number > n)
+	{
+		res = res + Math.floor(number / n);
+		n = n * 5;
+	}
+
+	return res;
+	
+}
+}
